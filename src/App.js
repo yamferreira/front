3909ed590ee51d504da2787bef4bf4975ed7ffdb
yamferreira <yamferreira6@gmail.com>
2025-03-27 17:10:16 -0300

    obtendo dados e cadastrando produto

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import Tabela from './tabela';
 
 function App() {
 
+  // Objeto produto
+  const produto = {
+    codigo : 0,
+    nome : '',
+    marca : ''
+  }
+
   //UseState
   const [btnCadastrar, setBtnCadastrar] = useState(true);
   const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
 
   //UseEffect
   useEffect(()=>{
@@ -16,11 +24,32 @@ function App() {
     .then(retorno_convertido => setProdutos(retorno_convertido));
   }, []);
 
+  // Obtendo os dados do formulario
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name] : e.target.value}); // Pega o valor do Obj produto
+  }
+
+  // Cadastrar produto
+  const cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar', { //Fazendo a requisição
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      console.log(retorno_convertido);
+    })
+     
+  }
+
   return (
     <div>
-      <p>{JSON.stringify(produtos)}</p>
-      <Formulario botao={btnCadastrar}/>
-      <Tabela/>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} />
+      <Tabela vetor={produtos} />
     </div>
   );
 }
